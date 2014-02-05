bf1287cd4cf0a19d21d62ab6831c2391fbf14922
kamiosako <k.masataka0409@gmail.com>
2014-02-05 20:58:50 +0900

    add custom.css to application.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ SampleApp::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( application.css )
   config.assets.precompile += %w( custom.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
