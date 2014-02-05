8973ed69f99707fcf176e425c5819326403a0c25
kamiosako <k.masataka0409@gmail.com>
2014-02-05 19:09:53 +0900

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -40,8 +40,8 @@ describe "Authentication" do
   describe "authorization" do
 
     describe "for non-signed-in users" do
-      let(:user) { FactoryGirl.create(:user) }
-   
+      let(:user) { FactoryGirl.create(:user) }  
+ 
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
@@ -58,15 +58,14 @@ describe "Authentication" do
         end
       end
 
-      describe "in the Microposts controller" do
-
+      describe "in the Relationships controller" do
         describe "submitting to the create action" do
-          before { post microposts_path }
+          before { post relationships_path }
           specify { expect(response).to redirect_to(signin_path) }
         end
 
         describe "submitting to the destroy action" do
-          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          before { delete relationship_path(1) }
           specify { expect(response).to redirect_to(signin_path) }
         end
       end
@@ -85,6 +84,16 @@ describe "Authentication" do
   
         describe "visiting the user index" do
           before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
           it { should have_title('Sign in') }
         end
       end
